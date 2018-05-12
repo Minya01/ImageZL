36490e8c55be76f5699d4fefad01e90f08edacd8
Summer <minya.su01@gmail.com>
2018-05-12 13:39:59 +0800

    setup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
+import setup from '././js/pages/setup'
 
-AppRegistry.registerComponent('ImageZL', () => App);
+// AppRegistry.registerComponent('ImageZL', () => App);
+AppRegistry.registerComponent('ImageZL', () => setup);
