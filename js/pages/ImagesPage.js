40f988c5107fb28c7b9c34b104e486891f3fb5e9
Summer <minya.su01@gmail.com>
2018-06-22 16:18:55 +0800

    add Icons

--- js/pages/ImagesPage.js
+++ js/pages/ImagesPage.js
@@ -11,16 +11,23 @@ import {
   Dimensions,
   ListView,
   TextInput,
-  LayoutAnimation
+  TouchableOpacity
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+var {height, width} = Dimensions.get('window');
 
 export default class ImagesPage extends Component{
     constructor(props){
         super(props);
     }
     render(){
-        return <View>
+        return <View style={{width:width,height:height}} >
             <Text>{this.props.albumName}</Text>
+            <TouchableOpacity style={{width:60, borderWidth:3, borderColor:'rgba(0,0,0,0.2)', alignItems:'center', justifyContent:'center', height:60, backgroundColor:'#ff585b', borderRadius:30, position:'absolute', bottom:70, right:20,zIndex:99}}>
+                <Icon name="add" size={22} color="#fff"/>
+            </TouchableOpacity>
         </View>
     }
 }
